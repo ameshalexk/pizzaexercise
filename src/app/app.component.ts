29b2f408e2454085e0b9e87bcc69b69fdb3c0f85
ameshalexk <ameshalexusa@gmail.com>
2022-02-09 13:35:45 -0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pizzaexercise';
+  public total = 15;
+
+
+  public toppings: any = {
+    pepperoni: 0.50,
+    sausage: 1.50,
+    peppers: 0.50,
+    onions: 0.50,
+    mushrooms: 0.75,
+    pineapple: 1.00,
+    bacon: 1.00,
+    ham: 0.75,
+    spinach: 0.75,
+    cheese: 0,
+    sauce: 0,
+  }
+
+
+  public toppingsList = Object.keys(this.toppings);
+  public toppingsListVal = Object.values(this.toppings);
+
+  public selection = [] as any;
+  public price = [] as any;
+
+  changeSelection(val: string) {
+    if (!this.selection[val] && this.selection.indexOf(val) === -1) {
+      this.selection.push(val);
+      this.total = this.total + this.toppings[val];
+
+    } else {
+      this.selection.pop(val);
+      this.total = this.total - this.toppings[val];
+
+    }
+  }
+
+
+
 }
